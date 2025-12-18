817e73bbd35eae153061e02fd5d3cd24c95af8c5
kavindhra <kavindhra.s2024eee@sece.ac.in>
2025-12-18 23:39:58 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import'./App.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'//hello
 import { CartProvider } from './context/CartContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  
   <CartProvider>
     <BrowserRouter>
       <App />
